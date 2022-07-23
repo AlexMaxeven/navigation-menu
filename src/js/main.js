74d848964590d2ded2d0064f2ee96ded6b7d8279
AlexSayer <106469761+AlexSayer@users.noreply.github.com>
2022-07-23 20:32:00 +0300

    production

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,9 @@
 // Меню бургер
 // находим элемент с которым будем работать
 const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
 // делаем проверку
 if (iconMenu){
-    const menuBody = document.querySelector('.menu__body');
     iconMenu.addEventListener("click", function(e) {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
@@ -23,7 +23,13 @@ if (menuLinks.length > 0) {
         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-        
+
+            if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
             window.scrollTo({
                 top: gotoBlockValue,
                 behavior: "smooth",
